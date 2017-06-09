123f144df4537285538e31b38e0b0baf44d2691e
Mark Hurlburt <mhurlburt@gmail.com>
2017-06-09 10:14:12 -0500

    restored changes to what Luke had done

--- server/app.js
+++ server/app.js
@@ -4,12 +4,8 @@ var path = require("path");
 
 app.set("port", (process.env.PORT || 5000));
 
-app.get("/*", function(req,res){
-     var file = req.params[0] || "/views/index.html";
-     res.sendFile(path.join(__dirname, "/public", file));
-});
-// app.use(express.static('server/public'));
-// app.use(express.static('server/public/views'));
+app.use(express.static('server/public'));
+app.use(express.static('server/public/views'));
 
 app.listen(app.get("port"), function(){
     console.log("Listening on port: ", app.get("port"));
