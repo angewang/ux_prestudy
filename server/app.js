0a9282279ecf33b2a722184a15050bf31eb3d9bd
Mark Hurlburt <mhurlburt@gmail.com>
2017-06-09 10:09:13 -0500

    trying older version of file serving

--- server/app.js
+++ server/app.js
@@ -4,8 +4,12 @@ var path = require("path");
 
 app.set("port", (process.env.PORT || 5000));
 
-app.use(express.static('server/public'));
-app.use(express.static('server/public/views'));
+app.get("/*", function(req,res){
+     var file = req.params[0] || "/views/index.html";
+     res.sendFile(path.join(__dirname, "/public", file));
+});
+// app.use(express.static('server/public'));
+// app.use(express.static('server/public/views'));
 
 app.listen(app.get("port"), function(){
     console.log("Listening on port: ", app.get("port"));
